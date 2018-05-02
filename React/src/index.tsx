d4bb074cbf60d11c3c9285ac6ceb2a57375543cb
tenwiz <nethe0002@gmail.com>
2018-05-02 15:09:18 +0400

    add component

--- React/src/index.tsx
+++ React/src/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import Hello from './components/Hello';
 
 ReactDOM.render(
-  <App />,
+  <Hello name="TypeScript" enthusiasmLevel={10} />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
