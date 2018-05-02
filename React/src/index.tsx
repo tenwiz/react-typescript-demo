71f864b229204edb0532d84854dc1a3a9f6470fc
tenwiz <nethe0002@gmail.com>
2018-05-02 15:09:44 +0400

    trailing "bang" syntax

--- React/src/index.tsx
+++ React/src/index.tsx
@@ -6,6 +6,6 @@ import Hello from './components/Hello';
 
 ReactDOM.render(
   <Hello name="TypeScript" enthusiasmLevel={10} />,
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root')!
 );
 registerServiceWorker();
